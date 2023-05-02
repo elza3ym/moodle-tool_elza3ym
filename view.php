3e0c7f74e81b3cf25deefa96ae9f21e84f9b1c4b
Mohamed Shehata <elza3ym@icloud.com>
2023-05-02 11:55:51 +0300

    add upgradeability

--- view.php
+++ view.php
@@ -1,4 +1,24 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   tool_elza3ym
+ * @copyright 2023, Mohamed Shehata <mohamed.shehata@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 require_once(__DIR__.'/../../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 $out = '';
@@ -6,7 +26,7 @@ $out .= html_writer::div('From view.php file', 'title', ['id' => 'titlehead']);
 $out .= '      <span lang="ar" class="multilang">your_content_here</span>
       <span lang="en" class="multilang">your_content_in_other_language_here</span>';
 $pagetitle = 'Text from view';
-$course_id = optional_param('id',0, PARAM_INT);
+$course_id = optional_param('id',0, PARAM_TEXT);
 $moodleUrl = new moodle_url('/tool/elza3ym/view.php');
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title($pagetitle);
@@ -19,7 +39,20 @@ $settingnode = $PAGE->settingsnav->add('setting', new moodle_url('/a/link/if/you
 $thingnode = $settingnode->add('Name of thing', new moodle_url('/a/link/if/you/want/one.php'));
 $thingnode->make_active();
 
+// DB Queries Testing.
+//$DB->set_debug(true);
 
+//$user = $DB->get_record('user', ['id' => $course_id], 'auth, email');
+//$user = $DB->get_record_sql('SELECT * FROM {user} WHERE id = ?', [$course_id]);
+//$user = $DB->get_field('user', 'auth', ['id' => $course_id]);
+//$user = $DB->get_fieldset_select('user', 'auth', 'id = ?', [$course_id]);
+$users = $DB->get_recordset('user');
+while ($users->valid() && $user = $users->current()) {
+  var_dump($user->email);
+  $users->next();
+}
+//var_dump($user);
+die();
 $output = $PAGE->get_renderer('tool_elza3ym');
 
 echo $output->header();
